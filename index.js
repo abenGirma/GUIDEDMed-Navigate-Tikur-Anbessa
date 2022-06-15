93322c6d81696e64c4c1467a487321e571a6668b
Abenezer <abenezergirma2@gmail.com>
2022-06-15 22:18:53 +0300

    Back to previous answerInline

--- index.js
+++ index.js
@@ -141,20 +141,7 @@ bot.on('inline_query', ctx => {
            
             //console.log(filtered);
             console.log(results);
-            //ctx.answerInlineQuery(results, {cache_time: 300});
-            /*ctx.answerInlineQuery({
-                type: 'article',
-                id: 'someID',
-                title: 'someTitle',
-                description: 'someDesc'
-            })
-            */
-            telegraf.answerInlineQuery({
-                type: 'article',
-                id: 'someID',
-                title: 'someTitle',
-                description: 'someDesc'
-            })
+            ctx.answerInlineQuery(results, {cache_time: 300});
             
         })
 
